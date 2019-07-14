b70a84a06300d4547210a3ccd401140cce4c9779
Alexander Ovchinnikov <aaovchinnikov@hse.ru>
2019-07-14 09:03:16 +0200

    Created simple window

--- templator/src/main/java/ru/hse/loganalysis/templator/App.java
+++ templator/src/main/java/ru/hse/loganalysis/templator/App.java
@@ -2,8 +2,7 @@ package ru.hse.loganalysis.templator;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -13,11 +12,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+    	BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
